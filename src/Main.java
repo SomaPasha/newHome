9ee07f38d2b32b5e048384ee69348c75d9f3532a
Pavel <pavel.kuzneczov.96@gmail.com>
2023-05-27 09:05:49 +0500

    Запись

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     private static final int SIZE = 3;
-    private static final int DOTS_TO_WIN= 3;
+    private static final int DOTS_TO_WIN = 3;
     private static final char DOT_EMPTY = '-';
     private static final char DOT_X = 'X';
     private static final char DOT_O = 'O';
@@ -14,31 +14,60 @@ public class Main {
     public static void main(String[] args) {
         initMap();
         writeMap();
-        while (true){
-            humanTurn();
-            printMap();
-            if(isWin())
-            {
-                System.out.println("Ура Вы выграли");
-                break;
-            }
-            if(isFull){
-                System.out.println("Ничия");
-                break;
-            }
+        //    while (true){
+        //  humanTurn();
+        writeMap();
+        //if(isWin())
+        // {
+        //  System.out.println("Ура Вы выграли");
+        //    break;
+        //  }
+        //if(isFull)
+        //{
+        //  System.out.println("Ничия");
+        //break;
+    }
 
-            uiTurn();
-            printMap();
-            if(isWin())
-            {
-                System.out.println("Выграл исскуственный интелект");
-                break;
-            }
-            if(isFull){
-                System.out.println("Ничия");
-                break;
+    // uiTurn();
+    //writeMap();
+    // if(isWin())
+    //     {
+    //       System.out.println("Выграл исскуственный интелект");
+    //     break;
+//}
+           // if(isFull)
+         //   {
+          //      System.out.println("Ничия");
+            //    break;
+            //}
+       // }
+     //       System.out.println("Игра окончена");
+
+
+
+    private static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j]=DOT_EMPTY;
             }
         }
-            System.out.println("Игра окончена");
     }
+
+    private static void writeMap() {
+        System.out.print(" ");
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(" "+(i+1));
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print(i+1+" ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
 }
